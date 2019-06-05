f6da74afcdf736bc38c19c26739fc35441f3fb2f
Prudhvi Gadiraju <gadiraju.prudhvi@gmail.com>
2019-06-04 17:59:33 -0700

    Initial Commit

---
+++ Tod2.0/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Tod2.0
+//
+//  Created by Prudhvi Gadiraju on 6/4/19.
+//  Copyright © 2019 Prudhvi Gadiraju. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
